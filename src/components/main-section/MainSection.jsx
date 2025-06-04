55612b892d2c09ae44c6733288038b57fa8d9bdb
Nikk45 <nicksin36@gmail.com>
2025-06-04 23:24:55 +0530

    search bar styling

--- src/components/main-section/MainSection.jsx
+++ src/components/main-section/MainSection.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { BiSearch } from "react-icons/bi";
+import { HiStar } from "react-icons/hi2";
 
+import './MainSection.css'
 
 export default function MainSection() {
   return (
@@ -19,7 +21,13 @@ export default function MainSection() {
                     <button>Find a Developer</button>
                 </div>
             </div>
-            <div className='review'></div>
+            <div className='review'>
+                <div className='review-title'>Trusted by 50k+ users</div>
+                <div className='rating'>
+                    <div className='stars'><HiStar/><HiStar/><HiStar/><HiStar/><HiStar/></div>
+                    <div className='review-count'>4.1/5 (14k) Reviews</div> 
+                </div>
+            </div>
         </div>
         <div className='right-section'></div>
     </div>
